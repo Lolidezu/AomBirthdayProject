5e5f4dcb56f759b6a5931b86103d3f3ac3dd7312
Lolidezu <112401678+Lolidezu@users.noreply.github.com>
2023-04-15 18:03:02 +0700

    V2

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
-/*function openVideo(link) {
-  var videoContainer = document.getElementById("video-container");
-  var videoFrame = document.createElement("iframe");
-  videoFrame.src = link.replace("/preview");
-  videoFrame.width = "560";
-  videoFrame.height = "315";
-  videoFrame.allow = "autoplay; fullscreen; picture-in-picture";
-  videoFrame.allowFullscreen = true;
-  videoFrame.style.border = "none";
-  videoContainer.innerHTML = "";
-  videoContainer.appendChild(videoFrame);
-}
+const vid22 = document.createElement('video');
+vid22.setAttribute('controls', true);
+vid22.setAttribute('style', 'position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); z-index: 9999; width: 800px; height: 600px;');
+vid22.setAttribute('id', 'my-video');
 
-*/
-function openVideo(link) {
-  var videoContainer = document.getElementById("video-container");
-  var videoFrame = document.createElement("iframe");
-  if (link.includes('drive.google.com')) {
-    link = link.replace("/view", "/preview");
-    var fileID = link.match(/\/d\/(.+)\//)[1];
-    link = `https://drive.google.com/file/d/${fileID}/preview`;
+document.querySelectorAll('.part').forEach(part => {
+  part.addEventListener('click', () => {
+    const partId = parseInt(part.id.replace('part', ''));
+    if (partId === -2) {
+      vid22.setAttribute('src', 'https://drive.google.com/uc?export=download&id=1ORmozfzFagbBa-6_WnNyY1yFkYX1aO1_');
+      vid22.play();
+      document.body.appendChild(vid22);
+    }
+    if (partId === -3) {
+      vid22.setAttribute('src', 'https://drive.google.com/uc?export=download&id=1qts73siYcpTO2J73V6g-n_LgUM87N4zr');
+      vid22.play();
+      document.body.appendChild(vid22);
+    }
+    if (partId === -1 || partId === -4) {
+      vid22.pause();
+      vid22.removeAttribute('src');
+      vid22.parentNode.removeChild(vid22);
+    }
+  });
+});
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape' && vid22.parentNode) {
+    vid22.pause();
+    vid22.removeAttribute('src');
+    vid22.parentNode.removeChild(vid22);
   }
-  videoFrame.src = link;
-  videoFrame.width = "560";
-  videoFrame.height = "315";
-  videoFrame.allow = "autoplay; fullscreen; picture-in-picture";
-  videoFrame.allowFullscreen = true;
-  videoFrame.style.border = "none";
-  videoContainer.innerHTML = "";
-  videoContainer.appendChild(videoFrame);
-}
-
+});
